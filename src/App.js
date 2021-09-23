2b3af841990bda617067bb654b624cdb455779a7
Sahil <hanss9545@gmail.com>
2021-09-24 02:22:33 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import Home from "./authentication/Home";
+// import Home from "./authentication/Home";
 import About from "./authentication/About";
 import Contact from "./authentication/Contact"
 import { BrowserRouter,Switch,Route} from "react-router-dom";
-import Category from "./authentication/Category";
+// import Category from "./authentication/Category";
 
 function App(){
     return (
         <>
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Home}></Route>
+                {/* <Route exact path="/" component={Home}></Route> */}
                 <Route path="/about" component={About} ></Route>
-                <Route path="/sports"><Category category="Sports"/></Route>
+                {/* <Route path="/sports"><Category category="Sports"/></Route>
                 <Route path="/Lifestyle"><Category category="lifestyle" /></Route>
                 <Route path="/Love"><Category category="love"/></Route>
                 <Route path="/Travel"><Category category="travel"/></Route>
@@ -20,7 +20,7 @@ function App(){
                 <Route path="/Art"><Category category="art"/></Route>
                 <Route path="/Photography"><Category category="photography"/></Route>
                 <Route path="/Design"><Category category="design"/></Route>
-                <Route path="/Dramas"><Category category="dramas"/></Route>
+                <Route path="/Dramas"><Category category="dramas"/></Route> */}
                 <Route path="/contact" component={Contact}/>
 
             </Switch>
